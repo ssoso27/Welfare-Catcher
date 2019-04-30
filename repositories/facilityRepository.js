dbf1ce3140a178d6017112b47329cfb453548840
ssoso27 <sohi8290@naver.com>
2019-05-01 03:21:16 +0900

    [F33] 시설 목록 조회 기능 구현
    - keyword 필터링 수정

--- repositories/facilityRepository.js
+++ repositories/facilityRepository.js
@@ -7,12 +7,8 @@ module.exports = class FacilityRepository {
 
     findAll(keyword='%', size=10, page=1) {
         var offset = (page-1)*size
-        var sql = `SELECT * FROM facilities WHERE name Like '${keyword}' LIMIT ${size} OFFSET ${offset};`;
+        var sql = `SELECT * FROM facilities WHERE name Like '%${keyword}%' LIMIT ${size} OFFSET ${offset};`;
 
-        console.log(keyword)
-        console.log(size)
-        console.log(page)
-        console.log(sql)
         return new Promise((resolve, reject) => {
             this.db.query(sql).then((result) => {
                 resolve(result);
