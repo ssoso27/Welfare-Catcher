c7ef8eed77367aab2a9b7c42e9b419e0b85acbb6
ssoso27 <sohi8290@gmail.com>
2019-06-12 07:41:13 +0900

    [hotfix] 자가진단 조건 변경

--- routes/services.js
+++ routes/services.js
@@ -5,15 +5,27 @@ const db = require('./../common/database.js')
 const ServiceRepository = require('./../repositories/serviceRepository.js');
 const serviceRepo = new ServiceRepository(db);
 
+const agegroups = require('./../enum/agegroup.json')
+const grades = require('./../enum/disability_grade.json')
+const types = require('./../enum/disability_type.json')
+
 router.get('/search', function(req, res, next) {
   return new Promise((resolve) => {
-    const disability_type = req.query.disability_type
-    const disability_grade = req.query.disability_grade
-    const age_group = req.query.age_group
+    const disability_type = types[req.query.disability_type]
+    const disability_grade = grades[req.query.disability_grade]
+    const age_group = agegroups[req.query.age_group]
     const size = req.query.size
     const page = req.query.page
 
     serviceRepo.search(disability_type, disability_grade, age_group, size, page).then(result => {
+      result.forEach((service, index, array) => {
+        service.age_group = agegroups[service.age_group]
+        service.disability_grade = grades[service.disability_grade]
+        service.disability_type = types[service.disability_type]
+        
+        array[index] = service
+      });
+
       res.send(result);
     })
     .catch(error => {
